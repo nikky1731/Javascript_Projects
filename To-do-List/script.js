883cd232ecbfdddc663c88c71e9ec9708d7d845a
nikky1731 <peddinikitha1731@gmail.com>
2024-02-16 14:28:24 +0530

    fourth commit for to do list

--- To-do-List/script.js
+++ To-do-List/script.js
@@ -11,5 +11,29 @@ function addTask(){
         let span =document.createElement("span");
         span.innerHTML= "\u00d7"
         li.appendChild(span)
-    }inputBox.value = " "
+    }
+    inputBox.value = " ";
+    saveData();
 }
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName  === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }
+    else if(e.target.tagName  === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
