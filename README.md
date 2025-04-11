e4d1023ff8424243598dc4f724b3e56f0c9bdf19
nikky1731 <peddinikitha1731@gmail.com>
2025-04-11 12:31:43 +0100

    fifth commit for Rock paper scissor and Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Javascript_Projectss
+
+Digital clock link : https://peaceful-gnome-0c0d98.netlify.app/
--- RockPaperScissor/script.js
+++ RockPaperScissor/script.js
@@ -7,4 +7,37 @@ const resultDisplay = document.getElementById("resultDisplay");
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     console.log(computerChoice);
+
+    if(playerChoice == computerChoice){
+        result = "It's a tie";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice == "scissors") ? "You win!" : "You Lose!";
+                break;
+            case "paper":
+                result = (computerChoice == "rock") ? "You win!" : "You Lose!";
+                break;
+            case "scissor":
+                result = (computerChoice == "paper") ? "You win!" : "You Lose!";
+                break;
+        }
+    }
+
+    resultDisplay.classList.remove("greenText", "redText")
+
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerChoice.textContent = `computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    switch(result){
+        case "You win!":
+            resultDisplay.classList.add("greenText");
+            break;
+        case "You Lose!":
+            resultDisplay.classList.add("redText");
+            break;
+    }
+
 }
